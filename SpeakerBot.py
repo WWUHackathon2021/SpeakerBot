a4e5073f848f4391406959deae24f809c5567372
unknown <joeymxiong@gmail.com>
2021-04-10 16:16:28 -0700

    merging with kyle

--- SpeakerBot.py
+++ SpeakerBot.py
@@ -28,38 +28,50 @@ arr = []
 #         await message.channel.send('Hello!')
 #     await bot.process_commands(message)
 
-@bot.command()
-async def join(ctx, url: str):
-    vc = discord.utils.get(ctx.guild.voice_channels, name="CF 416")
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+@bot.command(name = 'join')
+async def join(ctx,arg):
+    # connected = ctx.author.voice.channel
+    # await connected.connect()
+    vc = discord.utils.get(ctx.guild.voice_channels, name=arg)
+    print(vc)
+    await vc.connect()
 
-    if not voice.is_connected():
-        await vc.connect()
-        print("joined vc")
-    else:
-        print("already in vc")
-
-@bot.command()
+@bot.command(name = 'leave')
 async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("bot is not connected to vc")
+     await ctx.voice_client.disconnect()
+
+
+# async def join(ctx, url: str):
+#     vc = discord.utils.get(ctx.guild.voice_channels, name="CF 416")
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+#     if not voice.is_connected():
+#         await vc.connect()
+#         print("joined vc")
+#     else:
+#         print("already in vc")
+
+# @bot.command()
+# async def leave(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_connected():
+#         await voice.disconnect()
+#     else:
+#         await ctx.send("bot is not connected to vc")
     # if not vc is None:
     #     await voice.disconnect()
     # else:
     #     await ctx.send("The bot is not connected to a voice channel")
     
- @bot.command(name = 'testing')
- async def test(ctx):
-     test_quote = [
-         'David',
-         'Clothesline',
-         'David noooo do not pull the trigger'
-     ]
-     response = random.choice(test_quote)
-     await ctx.send(response)
+@bot.command(name = 'testing')
+async def test(ctx):
+    test_quote = [
+        'David',
+        'Clothesline',
+        'David noooo do not pull the trigger'
+    ]
+    response = random.choice(test_quote)
+    await ctx.send(response)
 
 # bot.add_command(test)
 
