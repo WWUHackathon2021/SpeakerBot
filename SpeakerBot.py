63e7b45d3a1eef437f8ee1f5017dea23d888f4ec
kyle-abrea <kidabrea@gmail.com>
2021-04-10 16:11:59 -0700

    join and leave

--- SpeakerBot.py
+++ SpeakerBot.py
@@ -31,49 +31,49 @@ arr = []
 @bot.command()
 async def join(ctx, url: str):
     vc = discord.utils.get(ctx.guild.voice_channels, name="CF 416")
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
-    if not voice.is_connected():
+    try:
         await vc.connect()
         print("joined vc")
-    else:
+        await ctx.send("SpeakerBot is now connected to a voice channel!")
+    except:
         print("already in vc")
+        await ctx.send("SpeakerBot is already connected to a voice channel!")
 
 @bot.command()
 async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("bot is not connected to vc")
-    # if not vc is None:
-    #     await voice.disconnect()
-    # else:
-    #     await ctx.send("The bot is not connected to a voice channel")
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    # await voice.disconnect()
+    try:
+        if voice.is_connected():
+            await voice.disconnect()
+            await ctx.send("SpeakerBot has left the voice channel")
+    except:
+        print("bot is not connected to vc")
+        await ctx.send("Unable to leave voice channel: SpeakerBot is not currently in a voice channel.")
     
- @bot.command(name = 'testing')
- async def test(ctx):
-     test_quote = [
-         'David',
-         'Clothesline',
-         'David noooo do not pull the trigger'
-     ]
-     response = random.choice(test_quote)
-     await ctx.send(response)
+#  @bot.command(name = 'testing')
+#  async def test(ctx):
+#      test_quote = [
+#          'David',
+#          'Clothesline',
+#          'David noooo do not pull the trigger'
+#      ]
+#      response = random.choice(test_quote)
+#      await ctx.send(response)
 
-# bot.add_command(test)
+# # bot.add_command(test)
 
-@bot.command(name = 'add')
-async def test(ctx, arg):
-    arr.append(arg)
+# @bot.command(name = 'add')
+# async def test(ctx, arg):
+#     arr.append(arg)
 
-@bot.command(name = "show")
-async def test(ctx):
-    await ctx.send(arr)
+# @bot.command(name = "show")
+# async def test(ctx):
+#     await ctx.send(arr)
 
-@bot.command(name = 'delete')
-async def test(ctx, arg):
-    arr.remove(arg)
+# @bot.command(name = 'delete')
+# async def test(ctx, arg):
+#     arr.remove(arg)
 
 bot.run(TOKEN)
 
