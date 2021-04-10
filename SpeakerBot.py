51086534e6a384ea51daff4664aac0d8f39f0005
kyle-abrea <kidabrea@gmail.com>
2021-04-10 13:38:01 -0700

    joined vc

--- SpeakerBot.py
+++ SpeakerBot.py
@@ -2,20 +2,36 @@ import discord
 import discord.ext
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+client = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+
+@client.command()
+async def play(ctx, url: str):
+    vc = discord.utils.get(ctx.guild.voice_channels, name="CF 416")
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    await vc.connect()
+
+# @client.command()
+# async def join(ctx, url: str):
+#     vc = discord.utils.get(ctx.guild.voice_channels, name="CF 416")
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     await vc.connect()
+
 client.run(TOKEN)
