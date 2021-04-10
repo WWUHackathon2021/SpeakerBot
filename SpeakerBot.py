226c97a77e00f17d53b57e52ff042c266d8d3560
unknown <joeymxiong@gmail.com>
2021-04-10 12:40:13 -0700

    got some commands implemented

--- SpeakerBot.py
+++ SpeakerBot.py
@@ -3,22 +3,36 @@ import discord.ext
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+import random
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-client = discord.Client()
-
-@client.event
+bot = commands.Bot(command_prefix='!')
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    await bot.process_commands(message)
 
-client.run(TOKEN)
+# bot.run(TOKEN)
+
+@bot.command(name = 'testing')
+async def test(ctx):
+    test_quote = [
+        'David',
+        'Clothesline',
+        'David noooo do not pull the trigger'
+    ]
+    response = random.choice(test_quote)
+    await ctx.send(response)
+
+# bot.add_command(test)
+bot.run(TOKEN)
 
