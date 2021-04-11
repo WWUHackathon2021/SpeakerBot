0e6f2669782e39a978497bdd0c11b4b49f61c33f
David <sonkdav@gmail.com>
2021-04-10 22:39:03 -0700

    Added a ready message to the bot and cleaned up code.

--- SpeakerBot.py
+++ SpeakerBot.py
@@ -11,32 +11,20 @@ from youtube_dl import YoutubeDL
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-client = discord.Client()
 
 bot = commands.Bot(command_prefix='!')
+
 arr = []
 players = {}
-# @bot.event
-# async def on_ready():
-#    print('We have logged in as {0.user}'.format(bot))
 
-# @bot.event
-# async def on_ready():
-#     print('We have logged in as {0.user}'.format(bot))
-
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-#     await bot.process_commands(message)
+@bot.event
+async def on_ready():
+   print('We have logged in as {0.user}'.format(bot))
+   await bot.get_channel(828834006829105162).send("SpeakerBot is ready to go! Make sure to use !help if you want the command list.")
 
 class General(commands.Cog):
     @commands.command(aliases=['j'], description="The bot will join whichever call you\'re currently in")
     async def join(self, ctx):
-        # vc = discord.utils.get(ctx.guild.voice_channels, name=arg)
         try:
             vc = ctx.author.voice.channel
         except:
@@ -63,7 +51,6 @@ class General(commands.Cog):
             print("bot is not connected to vc")
             await ctx.send("Unable to leave voice channel: SpeakerBot is not currently in a voice channel.")
 
-
 class Music(commands.Cog):
     @commands.command(aliases=['pl'], description="The bot will play the youtube link stated after \'!pl\'")
     async def play(self, ctx, url):
@@ -80,37 +67,6 @@ class Music(commands.Cog):
         else:
             await ctx.send("Already playing song")
             return
-
-    # async def play(ctx, url : str):
-    #     song_here = os.path.isfile("song.mp3")
-    #     try:
-    #         if song_here:
-    #             os.remove("song.mp3")
-    #     except PermissionError:
-    #         await ctx.send("Music is currently playing! Use !stop or !s to stop the song!")
-    #         return
-
-    #     vc = discord.utils.get(ctx.guild.voice_channels, name='CF 420')
-    #     voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
-
-    #     ydl_opts = {
-    #         'format': 'bestaudio/best',
-    #         'postprocessors': [{
-    #             'key': 'FFmpegExtractAudio',
-    #             'preferredcodec': 'mp3',
-    #             'preferredquality': '192',
-    #         }],
-    #     }
-
-    #     #FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
-    #     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #         ydl.download([url])
-    #     for file in os.listdir("./"):
-    #         if file.endswith(".mp3"):
-    #             os.rename(file, "song.mp3")
-        
-    #     voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
     @commands.command(aliases=['p'], description="The bot will pause the currently playing video.")
     async def pause(self, ctx):
@@ -154,12 +110,39 @@ async def show(ctx):
 async def delete(ctx, arg):
     arr.remove(arg)
 
-    # @bot.command()
-    # async def p(self,ctx,*,query):
-
 bot.add_cog(General())
 bot.help_command.cog = bot.cogs["General"]
 bot.add_cog(Music())
 bot.run(TOKEN)
 
+    # async def play(ctx, url : str):
+    #     song_here = os.path.isfile("song.mp3")
+    #     try:
+    #         if song_here:
+    #             os.remove("song.mp3")
+    #     except PermissionError:
+    #         await ctx.send("Music is currently playing! Use !stop or !s to stop the song!")
+    #         return
+
+    #     vc = discord.utils.get(ctx.guild.voice_channels, name='CF 420')
+    #     voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+
+    #     ydl_opts = {
+    #         'format': 'bestaudio/best',
+    #         'postprocessors': [{
+    #             'key': 'FFmpegExtractAudio',
+    #             'preferredcodec': 'mp3',
+    #             'preferredquality': '192',
+    #         }],
+    #     }
+
+    #     #FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    #     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    #         ydl.download([url])
+    #     for file in os.listdir("./"):
+    #         if file.endswith(".mp3"):
+    #             os.rename(file, "song.mp3")
+        
+    #     voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
